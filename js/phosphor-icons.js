2f68f76849ffe2ce63751c23275aad4cd876ab2c
izanmaizan <izanmaizan@gmail.com>
2023-02-21 19:20:13 +0700

    first commit

---
+++ js/phosphor-icons.js
@@ -0,0 +1,6 @@
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel ='stylesheet';
+link.type = 'text/css';
+link.href = 'http://unpkg.com/phosphor-icon@1.4.2/src/css/icon.css';
+head.appendChild(link);
